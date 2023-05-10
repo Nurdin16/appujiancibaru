380e5b50337102c3b63cd1d5ac882482d64f7b7a
Nurdin kurniawan <nurdink16@gmail.com>
2023-05-10 19:07:40 +0700

    menambah komentar di welcome

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -23,6 +23,7 @@ class Welcome extends CI_Controller
      */
     public function index()
     {
+        //menambah komentar
         $blade = new Blade(VIEWPATH, APPPATH . 'cache');
         echo $blade->make('form', [])->render();
     }
